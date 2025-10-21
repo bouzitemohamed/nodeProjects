5a4d8663c5def9ad326250c823ebbb18e780e67f
bouzzite mohammed <bouzzitemohammed2001@gmail.com>
2025-10-21 11:04:27 +0200

    ajouter les reponses sur les question d'activities en form de commentaire sur les fichiers app.js

--- activities1/app.js
+++ activities1/app.js
@@ -1,3 +1,14 @@
+/* 
+1)Dans Node.js (et JavaScript en général), 
+si plusieurs fichiers déclarent des variables, 
+fonctions ou classes portant le même nom, 
+elles peuvent entrer en conflit — mais cela dépend du contexte.
+2)dnas un projet reale Plusieurs développeurs travaillent sur le même code.
+Certains peuvent déclarer des variables ou fonctions avec les mêmes noms,
+est va creer un probleme.
+3)Node.js résout ce problème il utilise un système de modules,
+ basé sur le CommonJS (et aussi les modules ES depuis ECMAScript 2015).
+*/
 const PI=3.14;
 function calculerAire(rayon){
     return PI * rayon * rayon;
--- activities4/app.js
+++ activities4/app.js
@@ -1,3 +1,17 @@
+/**
+ * 1)
+   contactService.js:Gère la logique métier : ajout et liste des contacts (données et traitement).
+   utils/format.js:Gère le formatage de l’affichage d’un contact (présentation des données).
+   app.js:C’est le point d’entrée de l’application : il coordonne les autres modules, exécute la logique et affiche le résultat.
+   *2)
+   Parce que cela suit le principe de séparation des responsabilités (Separation of Concerns).
+   Chaque fichier a une tâche précise et indépendante :
+   Le formatage peut changer sans casser la logique métier.
+   La logique (ajout/liste) peut évoluer sans toucher l’affichage.
+   Le point d’entrée reste simple et clair.
+   *3)
+   Réutilisabilité,Évolutivité,Testabilité,Lisibilité
+ */
 const {ajouterContact,listerContacts}=require('./contactservice');
 const formaterContact=require('./utils/format');
 ajouterContact("alice","067584945");
